661e4ad655ffd0993b7e02b4ea848f93de50826b
Franccesco <franccesco_@hotmail.com>
2023-12-10 15:16:38 -0300

    trocando gerenciador de pacotes e atualizando pacotes

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,17 @@ import { useEffect, useState } from 'react';
 import Pokecard from './components/Pokecard';
 import { Busca } from './services/api';
 import './styles/index.css';
-import SearchBar from './components/SearchBar/SearchBar';
+import SearchBar from './components/SearchBar';
 
 const App = (): JSX.Element => {
   const [pokemons, setPokemons] = useState<any>([]);
   const [filteredPokemon, setFilteredPokemon] = useState<any[]>([]);
-
 
   useEffect(() => {
     Busca('?limit=493', setPokemons);
   }, []);
 
   const handleSearch = (searchTerm: string) => {
-    // Lógica para filtrar os Pokémon com base no termo de pesquisa
     const filteredResults = filteredPokemon.filter((pokemon) =>
       pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
